465f90eb308d08e0c7ab8f061a805a40d9abb5ba
Mariana Gogol <marianavynnyk610@gmail.com>
2020-05-02 17:12:55 +0200

    Updates

---
+++ precache-manifest.6aebcdf976bd6a6b42c115bfacf98123.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "693e25d69ed80748993cb94c569466bf",
+    "url": "/kenzap/index.html"
+  },
+  {
+    "revision": "f7ecd11c9accd9a4ff15",
+    "url": "/kenzap/static/css/2.d20b2b2f.chunk.css"
+  },
+  {
+    "revision": "185201445d1b9bec592c",
+    "url": "/kenzap/static/css/main.e567957f.chunk.css"
+  },
+  {
+    "revision": "f7ecd11c9accd9a4ff15",
+    "url": "/kenzap/static/js/2.0315006d.chunk.js"
+  },
+  {
+    "revision": "060e89aaa9609450046671e089c663a4",
+    "url": "/kenzap/static/js/2.0315006d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "185201445d1b9bec592c",
+    "url": "/kenzap/static/js/main.c9d05976.chunk.js"
+  },
+  {
+    "revision": "de6a512629aaae1f629e",
+    "url": "/kenzap/static/js/runtime-main.fb182357.js"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/kenzap/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/kenzap/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/kenzap/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/kenzap/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/kenzap/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/kenzap/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/kenzap/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/kenzap/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/kenzap/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/kenzap/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/kenzap/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/kenzap/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/kenzap/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/kenzap/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/kenzap/static/media/fa-solid-900.cdfec5cf.woff"
+  }
+]);
